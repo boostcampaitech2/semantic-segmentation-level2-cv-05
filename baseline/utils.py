c96a51c3b2ef48a3495117d4a992b8aa1a1c2d3f
lkm2835 <lkm2835@gmail.com>
2021-10-20 13:33:24 +0900

    set_seed os

--- baseline/utils.py
+++ baseline/utils.py
@@ -75,6 +75,7 @@ def set_seed(seed) :
     torch.cuda.manual_seed_all(seed) # if use multi-GPU
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+    os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
     random.seed(seed)
 
